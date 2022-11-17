e5a1c88243bfef5387deaadc70efdaec8aba4f4a
Kamil Kucharski <kamil.kucharski@makimo.pl>
2022-11-17 15:43:52 +0100

    Use black instead of pylint

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/hello", methods=["GET"])
 def hello():
     return "Hello", 200
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=9000, debug=True)
+    app.run(host="0.0.0.0", port=9000, debug=True)
